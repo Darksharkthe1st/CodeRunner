2e5ac052757196acd6c58ab45b611ad53ba81d0f
Farhan Kittur <kitturfarhan@gmail.com>
2025-12-25 22:08:02 -0400

    Create temporary execution code

--- src/main/java/com/cr/coderunner/CodeSubmission.java
+++ src/main/java/com/cr/coderunner/CodeSubmission.java
@@ -1,32 +1,94 @@
 package com.cr.coderunner;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.InputMismatchException;
+import java.util.List;
+
 public class CodeSubmission {
     public String code;
+    public String language;
     public boolean success;
     public double runtime;
+
     //TODO: Add string to represent code language
 
-    public CodeSubmission(String code) {
+    public CodeSubmission(String code, String language) {
         this.code = code;
+        this.language = language;
         this.success = false;
         this.runtime = -1;
     }
 
-    public void buildAndRun() {
+    public String getExtensionByLang(String language) {
+        if (language.equals("Java")) {
+            return ".java";
+        } else {
+            return "none found";
+        }
+    }
+
+    public void buildAndRun() throws IOException {
 
         //Get the current directory
-        String userDir = System.getProperty("user.dir");
+        File userDir = new File(System.getProperty("user.dir"));
+        File execDir = new File(userDir, ".test");
 
-        //TODO: create a temporary code file with an appropriate filename
+        //Check if the extension is valid and save it
+        String extension = getExtensionByLang(this.language);
+        if (extension.equals("none found")) {
+            throw new InputMismatchException(this.language + " is not a supported language in CodeRunner.");
+        }
 
-        //TODO: use ProcessBuilder to run code file accordingly (javac xxx)
+        //Make a new directory if needed
+        if (execDir.mkdir()) {
+            System.out.println("testing directory not detected; new directory created.");
+        }
+
+        //Create a temporary code file with an appropriate filename
+        File codeFile = new File(execDir, "file" + extension);
+        if (!codeFile.createNewFile()) {
+            System.out.println("Code file already exists; overwriting.");
+        }
+
+        //Overwrite existing text file
+        Files.writeString(codeFile.toPath(),this.code, StandardOpenOption.WRITE);
+
+        //Create an output file for the code to write into
+        File outputFile = new File(execDir, "output.txt");
+        if (!outputFile.createNewFile()) {
+            System.out.println("Output file already exists; overwriting.");
+        }
+
+        //Prepare ProcessBuilder to run code file accordingly (e.g. java xxx.java)
         ProcessBuilder p = new ProcessBuilder();
+        p.directory(execDir);
+        p.redirectOutput(outputFile);
+
+        //Use different execution methods for different languages
+        if (language.equals("Java")) {
+            p.command("java", codeFile.getAbsolutePath());
+        }
 
         //TODO: use Docker to ensure dev env has all needed build tools
 
         //TODO: funnel ProcessBuilder outputs into a variable somehow
 
         //TODO: Add evaluation of code outputs by checking variable equality (start with ints)
+        System.out.println("Here is the output: ");
+        List<String> fileLines = Files.readAllLines(outputFile.toPath());
+
+        //Use string builder to convert output list of strings into readable format
+        StringBuilder fullOutput = new StringBuilder("");
+        for (String line : fileLines) {
+            fullOutput.append(line);
+            fullOutput.append("\n");
+        }
+
+        //Print out full output for now for testing purposes
+        System.out.println(fullOutput);
 
         //Set success to true by default for now
         this.success = true;
