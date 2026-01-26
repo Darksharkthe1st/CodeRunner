a8ad9ba9d93088b82dab2942742fa8dc60279d68
DarksharkThe1st <kitturfarhan@gmail.com>
2026-01-26 19:50:22 +0000

    Modify properties and webconfig to avoid duplicate CORS policy

--- src/main/java/com/cr/coderunner/service/WebConfig.java
+++ src/main/java/com/cr/coderunner/service/WebConfig.java
@@ -14,10 +14,11 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://code-runner-eta.vercel.app")
+                        .allowedOrigins("https://code-runner-eta.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(true)
+			.maxAge(3600);
             }
         };
     }
